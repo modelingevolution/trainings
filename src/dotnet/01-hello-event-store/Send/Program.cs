b88a56689db1a1019bcec0610162c6ee43d4aec3
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-03-05 13:10:58 +0100

    git ignore + Send Program

---
+++ src/dotnet/01-hello-event-store/Send/Program.cs
@@ -0,0 +1,52 @@
+﻿using EventStore.Client;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace Send
+{
+    class MessageSent
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Text { get; set; }
+    }
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var topicId = args[0].ToGuid();
+            string line = "";
+
+            var client = CreateClient();
+
+            while (line != "exit")
+            {
+                line = Console.ReadLine();
+
+                var evt = new MessageSent { Text = line };
+
+                var eventData = new EventData(
+                    Uuid.NewUuid(),
+                    nameof(MessageSent),
+                    JsonSerializer.SerializeToUtf8Bytes(evt)
+                );
+
+                await client.AppendToStreamAsync(
+                    $"Thread-{topicId}",
+                    StreamState.Any,
+                    new[] { eventData }
+                );
+            }
+        }
+
+        static EventStoreClient CreateClient()
+        {
+            const string connectionString = "esdb://admin:changeit@localhost:2113?tls=false&tlsVerifyCert=false";
+
+            var settings = EventStoreClientSettings.Create(connectionString);
+
+            var client = new EventStoreClient(settings);
+            return client;
+        }
+    }
+}
