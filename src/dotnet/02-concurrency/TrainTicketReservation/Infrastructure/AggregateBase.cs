61062b9949f5e5550cc6f6c70cd1d9509fec7398
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-03-10 20:38:51 +0100

    More cleanup

--- src/dotnet/02-concurrency/TrainTicketReservation/Infrastructure/AggregateBase.cs
+++ src/dotnet/02-concurrency/TrainTicketReservation/Infrastructure/AggregateBase.cs
@@ -5,27 +5,33 @@ public abstract class AggregateBase<TState>(Guid id)
 
 {
     private readonly List<object> _pendingEvents = new();
+
+    protected TState State { get; private set; } = new();
+
+    public Guid Id => id;
+    public long Age { get; private set; } = -1;
+
+    public IReadOnlyList<object> PendingEvents => _pendingEvents;
+
     protected void AppendEvent(object ev)
     {
         _pendingEvents.Add(ev);
         Apply(ev);
     }
 
-    private TState _state = new();
-    protected TState State => _state;
-    private long _age = -1;
-    public Guid Id => id;
-    public long Age => _age;
-    public IReadOnlyList<object> PendingEvents => _pendingEvents;
     public async Task Rehydrate(IAsyncEnumerable<object> events)
     {
         await foreach (var e in events)
         {
-            _state = Apply(e);
-            _age += 1;
+            State = Apply(e);
+            Age += 1;
         }
     }
 
     protected abstract TState Given(TState state, object ev);
-    private TState Apply(object ev) => Given(_state, ev);
+
+    private TState Apply(object ev)
+    {
+        return Given(State, ev);
+    }
 }
